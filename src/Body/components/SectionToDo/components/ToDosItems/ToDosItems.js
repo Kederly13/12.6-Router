7a30593930178e60dc02c7ad6ff6c052e8f9ea8d
serhiiderkach <serhiiderkach1995@gmail.com>
2023-03-27 22:06:24 -0500

    added

--- src/Body/components/SectionToDo/components/ToDosItems/ToDosItems.js
+++ src/Body/components/SectionToDo/components/ToDosItems/ToDosItems.js
@@ -1,15 +1,35 @@
+import { useState } from 'react';
+
 import { ToDoItem } from './components/ToDoItem/ToDoItem.js';
 import { todosData } from './data/todosData.js';
 
-const ToDosItems = () => (
-    todosData.map(({ id, text, completed }) => (
-        <ToDoItem
-            description={text}
-            key = {id}
-            completed = {completed}
-        />
-    ))
-);
+const ToDosItems = () => {
+    const handleChange = ({id}) => {
+        console.log('changed', id)
+    }
+
+    const [toDos, setActive] = useState(todosData)
+
+    const { id, text, completed, ...toDoItems } = toDos;
+
+
+    
+    const activeTodos = todosData.map(({ id, text, completed }) => (
+            <ToDoItem
+                description={text}
+                key={id}
+                completed={completed}
+                handleChange={()=>handleChange({id})}
+            />
+        ))
+        
+
+    return (
+        activeTodos
+    )
+    
+};
+
 
 
 export { ToDosItems };
--- src/Body/components/SectionToDo/components/ToDosItems/components/ToDoItem/ToDoItem.js
+++ src/Body/components/SectionToDo/components/ToDosItems/components/ToDoItem/ToDoItem.js
@@ -1,9 +1,9 @@
 import './ToDoItem.css';
 
-const ToDoItem = ({ description, completed }) => (
+const ToDoItem = ({ description, completed, handleChange }) => (
     <div className="todo-item">
         <h2 className='title'>Attention! Headline</h2>
-        <input className='input' type="checkbox" defaultChecked={completed}/>
+        <input className='input' type="checkbox" onChange={handleChange} defaultChecked={completed}/>
         <p className='description'>{description}</p>       
     </div>
 );
